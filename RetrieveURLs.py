0beccc646ec7a60c5f4f3af7d06490f82718d372
jmglueck <caravaggio4lyfe@gmail.com>
2020-03-01 11:55:42 -0800

    Update RetrieveURLs.py
    Put show_results function in retrieveURLs.py

--- RetrieveURLs.py
+++ RetrieveURLs.py
@@ -4,6 +4,7 @@ import numpy as np
 from numpy import dot
 from numpy.linalg import norm
 import operator
+from tkinter import *
 
 """ if the database doesn't exist, it will be created """
 conn = sqlite3.connect('Inverted.db')
@@ -11,16 +12,19 @@ print ("Opened database successfully")
 
 cursor = conn.cursor()     
 
-searchWord = input("Enter your search terms: ")
-terms = searchWord.split(" ")
-print(terms)
-  
-list_of_URLs = {}
-i = 0
-for term in terms:
-    q = []
-    d = []
-    Query = "SELECT Token, File, Frequency, IDF, TF_IDF, URL from UCIIndexFinal WHERE Token = '" + term + "'"
+#searchWord = input("Enter your search terms: ")
+#terms = searchWord.split(" ")
+#print(terms)
+
+def show_results(the_entry, the_root): 
+    search_query = the_entry.get()
+    terms = search_query.split(" ")
+    list_of_URLs = {}
+    i = 0
+    for term in terms:
+        q = []
+        d = []
+        Query = "SELECT Token, File, Frequency, IDF, TF_IDF, URL from UCIIndexFinal WHERE Token = '" + term + "'"
     cursor = conn.execute(Query)
     for row in cursor:
         #print for testing, then comment out 
@@ -37,22 +41,26 @@ for term in terms:
                 d.append(row[2])
                 if(row[0] in terms):
                     q.append((1 + np.log10(1)) * np.log10(37497/row[2]))
-                     #print("query term position in doc", row[0])
                 else:
                     q.append(0)
-                
-            #print(q)
-            #print(d)
             cos_sim = dot(q, d)/(norm(q)*norm(d))
-            #print("cosine sim - ", cos_sim, " - ", URL, "\n")
             list_of_URLs[URL] = cos_sim
             print(i)
         #break;
+    offset = 4
+    list_of_results = []
+    sortedDesc = sorted(list_of_URLs.items(), key=operator.itemgetter(1), reverse=True)[:10]
+    for url in sortedDesc.keys():
+        label = Label(the_root)
+        label["text"] = url 
+        list_of_results.append(label)
+        label.grid(row=offset, column=0)
+        offset += 1
 
-sortedDesc = sorted(list_of_URLs.items(), key=operator.itemgetter(1), reverse=True)[:10]
+#sortedDesc = sorted(list_of_URLs.items(), key=operator.itemgetter(1), reverse=True)[:10]
 # Showing cosine similarity for testing, need to show only URLs line by line
-print("Retrieved ", i, " URLs")
-print("Below are the top 10 results of your query: ", sortedDesc) 
+#print("Retrieved ", i, " URLs")
+#print("Below are the top 10 results of your query: ", sortedDesc) 
 
 
 conn.close()
