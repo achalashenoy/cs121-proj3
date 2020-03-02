68636878b503a94edc77128f069d78b9f0e10769
Achala Shenoy <achalashenoy@gmail.com>
2020-03-01 20:50:35 -0800

    added champion lists and html weight top 20

--- RetrieveURLs.py
+++ RetrieveURLs.py
@@ -8,7 +8,7 @@ from tkinter import *
 import RetrieveURLs
 
 """ if the database doesn't exist, it will be created """
-conn = sqlite3.connect('Inverted3.db')
+conn = sqlite3.connect('Inverted.db')
 print ("Opened database successfully")
 
 cursor = conn.cursor()     
@@ -21,32 +21,44 @@ def show_results(the_entry, the_root):
     search_query = the_entry.get()
     terms = search_query.split(" ")
     list_of_URLs = {}
-    i = 0
+    #i = 0
+    N = 37497
     for term in terms:
         q = []
         d = []
-        Query = "SELECT Token, File, Frequency, IDF, TF_IDF, URL from UCIIndexFinal WHERE Token = '" + term + "'"
-    cursor = conn.execute(Query)
-    for row in cursor:
-        #print for testing, then comment out 
-        #print (row[0], " - ", row[1], row[2], row[3], row[4], row[5], "\n")
-        file = row[1]
-        URL = row[5]
-        if (URL not in list_of_URLs):
-            # Comment out after testing
-            i = i + 1
-            Query = "SELECT Token, IDF, TF_IDF FROM UCIIndexFinal WHERE File = '" + file + "'"
-            cursor = conn.execute(Query)
-            for row in cursor:
-                #print(row[0], row[2])
-                d.append(row[2])
-                if(row[0] in terms):
-                    q.append((1 + np.log10(1)) * np.log10(37497/row[2]))
-                else:
-                    q.append(0)
+        Query = "SELECT Token, File, Frequency, IDF, TF_IDF, URL FROM UCIIndexFinal WHERE Token = '" + term + "' \
+            ORDER BY (TF_IDF + HTML_weight) DESC LIMIT 1000"
+        cursor = conn.execute(Query)
+        for row in cursor:
+            #print for testing, then comment out 
+            #print (row[0], " - ", row[1], row[2], row[3], row[4], row[5], "\n")
+            file = row[1]
+            URL = row[5]
+            if (URL not in list_of_URLs):       
+                # Comment out after testing
+                #i = i + 1
+                Query = "SELECT Token, IDF, TF_IDF, HTML_weight FROM UCIIndexFinal WHERE File = '" + file + "'"
+                cursor = conn.execute(Query)
+                
+                for row in cursor:
+                    #print(row[0], row[2])
+                    #d.append(row[2])
+                    if(row[0] in terms):
+                        # TD_IDF and HTML_weight of the query term in the document vector
+                        d.append(row[2] + row[3])
+                        
+                        # TD_IDF of the query term in the query vector
+                        q.append((1 + np.log10(1)) * np.log10(N/(row[2])))
+                        #print("query term position in doc", row[0])
+                    #else:
+                        #q.append(0)
+                
+            #print(q)
+            #print(d)
             cos_sim = dot(q, d)/(norm(q)*norm(d))
+            #print("cosine sim - ", cos_sim, " - ", URL, "\n")
             list_of_URLs[URL] = cos_sim
-            print(i)
+            #print(i)
         #break;
     offset = 4
     header = Label(the_root)
